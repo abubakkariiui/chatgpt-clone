d1b6b064032672939a1a2c51139c9d75918cd27b
abubakkariiui <abubakkarmit@gmail.com>
2023-02-27 10:52:03 +0500

    add icon

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>ChatGpt Clone</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/chatgpt-icon.webp" />
     </>
   )
 }
